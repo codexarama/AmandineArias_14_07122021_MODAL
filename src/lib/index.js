486d9a3153473c19dc2dbdfd268d87679c7f1d62
Amandine Arias <amandine.arias@gmail.com>
2022-01-06 12:44:19 +0100

    feat (Lib) : imports & exports for package publication

---
+++ src/lib/index.js
@@ -0,0 +1,29 @@
+// DOCUMENTATION
+import FEATURES from './documentation/codexarama-modal_FEATURES.PNG'
+import PROPS from './documentation/codexarama-modal_PROPS.PNG'
+import MORE from './documentation/codexarama-modal_MORE.PNG'
+import EXEMPLE from './documentation/codexarama-modal_EXEMPLE.PNG';
+
+// ASSETS
+import close from './assets/close.png';
+import check from './assets/check.png';
+import thumbUp from './assets/thumb-up.png'
+
+// STYLES
+import './styles/modal.css'
+import './styles/exemple.css'
+
+// COMPONENT
+import useModal from './component/useModal';
+import Modal from './component/Modal';
+
+// EXPORTS
+export { close };
+export { check };
+export { thumbUp }
+export { useModal };
+export { Modal };
+export { FEATURES }
+export { PROPS }
+export { MORE }
+export { EXEMPLE }
