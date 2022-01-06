50e8e8ae5a2fc40780b2ede760f4a6845e54d00f
Amandine Arias <amandine.arias@gmail.com>
2022-01-06 13:37:04 +0100

    feat (package) : npm publish (creating dist/ folder)

---
+++ dist/index.js
@@ -0,0 +1,83 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "EXEMPLE", {
+  enumerable: true,
+  get: function get() {
+    return _codexaramaModal_EXEMPLE.default;
+  }
+});
+Object.defineProperty(exports, "FEATURES", {
+  enumerable: true,
+  get: function get() {
+    return _codexaramaModal_FEATURES.default;
+  }
+});
+Object.defineProperty(exports, "MORE", {
+  enumerable: true,
+  get: function get() {
+    return _codexaramaModal_MORE.default;
+  }
+});
+Object.defineProperty(exports, "Modal", {
+  enumerable: true,
+  get: function get() {
+    return _Modal.default;
+  }
+});
+Object.defineProperty(exports, "PROPS", {
+  enumerable: true,
+  get: function get() {
+    return _codexaramaModal_PROPS.default;
+  }
+});
+Object.defineProperty(exports, "check", {
+  enumerable: true,
+  get: function get() {
+    return _check.default;
+  }
+});
+Object.defineProperty(exports, "close", {
+  enumerable: true,
+  get: function get() {
+    return _close.default;
+  }
+});
+Object.defineProperty(exports, "thumbUp", {
+  enumerable: true,
+  get: function get() {
+    return _thumbUp.default;
+  }
+});
+Object.defineProperty(exports, "useModal", {
+  enumerable: true,
+  get: function get() {
+    return _useModal.default;
+  }
+});
+
+var _codexaramaModal_FEATURES = _interopRequireDefault(require("./documentation/codexarama-modal_FEATURES.PNG"));
+
+var _codexaramaModal_PROPS = _interopRequireDefault(require("./documentation/codexarama-modal_PROPS.PNG"));
+
+var _codexaramaModal_MORE = _interopRequireDefault(require("./documentation/codexarama-modal_MORE.PNG"));
+
+var _codexaramaModal_EXEMPLE = _interopRequireDefault(require("./documentation/codexarama-modal_EXEMPLE.PNG"));
+
+var _close = _interopRequireDefault(require("./assets/close.png"));
+
+var _check = _interopRequireDefault(require("./assets/check.png"));
+
+var _thumbUp = _interopRequireDefault(require("./assets/thumb-up.png"));
+
+require("./styles/modal.css");
+
+require("./styles/exemple.css");
+
+var _useModal = _interopRequireDefault(require("./component/useModal"));
+
+var _Modal = _interopRequireDefault(require("./component/Modal"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
